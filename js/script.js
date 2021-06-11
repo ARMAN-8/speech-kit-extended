030cdf479715a0838c643e8b1d9eb2bc64363b6d
RAZOR <alimnurarman@gmail.com>
2021-06-11 15:15:47 +0600

    fisrt commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+/*var swiperMobile = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+});*/
